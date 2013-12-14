44690f7055d91930117e298e0ff50f540c8896b8
Vivien Sin <viviensinym@gmail.com>
2013-12-14 14:18:47 -0800

    Setting up

---
+++ junto/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Junto::Application.config.secret_key_base = '0ac05c95c88b0d254f8f9f8b109593300f7dd4bd9992b3c7a92b3b3f8ee42263d7cc51757bb5ce7a4642348b0336dcb1caa8b18df2e20b6fa2c4897766c99376'
---
+++ junto/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Junto::Application.config.session_store :cookie_store, key: '_junto_session'
